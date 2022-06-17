9f3b4e2ad928234bbaf8b5801c85138be43c90a3
Sydorenko Vladyslav <cayceworld@outlook.com>
2022-06-17 13:18:44 +0100

    Fix navbar locked

--- src/js/components/navbar.js
+++ src/js/components/navbar.js
@@ -27,6 +27,7 @@ function burgerMenu() {
     link.addEventListener('click', () => {
       menu.classList.remove('active');
       burger.classList.remove('active-burger');
+      body.classList.remove('locked');
     });
   }
 
